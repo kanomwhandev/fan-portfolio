6b76b3ef84f48f282d46810dd53364797821452d
Affan Samaeng <kanomdev@outlook.com>
2024-10-04 19:53:44 +0700

    change cv

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
       <div className="container mx-auto flex justify-between items-center  ">
         {/* logo */}
         <Link href="/">
-          <h1 className="text-4xl font-semibold  ">
+          <h1 className="text-4xl font-semibold ">
             Portfolio<span className="text-accent">.</span>
           </h1>
         </Link>
